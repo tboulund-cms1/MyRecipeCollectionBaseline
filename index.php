4d56eae1c0b838e1d5b73c4c5a051986bd6f1ccb
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2022-05-04 09:38:35 +0200

    Implemented solution

--- index.php
+++ index.php
@@ -4,18 +4,13 @@
         <div class="white-box col-6 offset-3">
             <!-- TODO: Implement a custom post type named recipe and make a custom Wordpress loop that prints all recipes in the list in stead of this hardcoded dummy list. -->
             <ul class="mb-0">
-                <li>
-                    <a href="#">Fried pork with parsley sauce</a>
-                </li>
-                <li>
-                    <a href="#">Hamburgers</a>
-                </li>
-                <li>
-                    <a href="#">Chocolate chip cookies</a>
-                </li>
-                <li>
-                    <a href="#">Spaghetti bolognese</a>
-                </li>
+                <?php $recipeLoop = new WP_Query(array("post_type" => "recipe", "posts_per_page" => -1)) ?>
+                    <?php while($recipeLoop->have_posts()): $recipeLoop->the_post() ?>
+                        <li>
+                            <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
+                        </li>
+                    <?php endwhile ?>
+                <?php wp_reset_postdata() ?>
             </ul>
         </div>
     </div>
